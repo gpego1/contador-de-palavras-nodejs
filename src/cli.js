e04ffd92e0be317fa2ecd141859a1bb6a41b448d
gpego1 <gpego78@gmail.com>
2026-01-13 19:27:50 -0300

    async await e promise

---
+++ src/cli.js
@@ -0,0 +1,44 @@
+import fs from 'fs';
+import trataErros from './erros/funcoesErro.js';
+import { contaPalavras } from './index.js';
+
+const filesPath = process.argv;
+const link = filesPath[2];
+const endereco = filesPath[3];
+
+fs.readFile(link, 'utf-8', (err, text) => {
+    try{
+        if (err) throw err;
+        const resultado = contaPalavras(text);
+        criaESalvaArquivo(resultado, endereco);
+    } catch(err){
+        trataErros(err);
+    }
+})
+
+// async function criaESalvaArquivo(listaPalavras, endereco) {
+//     const arquivoNovo = `${endereco}/resultado.txt`;
+//     const textoPalavras = JSON.stringify(listaPalavras);
+//     try {
+//         await fs.promises.writeFile(arquivoNovo, textoPalavras);
+//         console.log('arquivo criado');
+//     } catch(erro) {
+//         throw erro;
+//     }
+// }
+
+function criaESalvaArquivo(listaPalavras, endereco) {
+    const arquivoNovo = `${endereco}/resultado.txt`;
+    const textoPalavras = JSON.stringify(listaPalavras);
+
+    fs.promises.writeFile(arquivoNovo, textoPalavras)
+        .then(() => {
+            // processamento feito com o resultado da promessa
+            console.log('Arquivo criado com sucesso!')
+        })
+        .catch((erro) => {
+            throw erro;
+    })
+        .finally(() => console.log('Operação finalizada'))
+
+}
--- src/index.js
+++ src/index.js
@@ -1,47 +1,33 @@
-const fs = require('fs');
-const trataErros = require('./erros/funcoesErro.js')
-
-const filesPath = process.argv;
-const link = filesPath[2];
-
-fs.readFile(link, 'utf-8', (err, text) => {
-    try{
-        if (err) throw err;
-        contaPalavras(text);
-    } catch(err){
-        trataErros(err);
-    }
-})
-
-function contaPalavras(text) {
+export function contaPalavras(text) {
     const paragrafos = extraiParagrafos(text);
     const contagem = paragrafos
         .flatMap((paragrafo) => {
-            if(!paragrafo) return [];
+            if (!paragrafo) return [];
             return verificaPalavrasDuplicadas(paragrafo);
         })
-    console.log(contagem);
+    return contagem;
 }
 
-function extraiParagrafos(text) {
-    return paragrafos = text.toLowerCase().split('\n');
-}
+    function extraiParagrafos(text) {
+        return text.toLowerCase().split('\n');
+    }
 
 
-function limpaPalavras(palavra) {
-    return palavra.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,'');
-}
+    function limpaPalavras(palavra) {
+        return palavra.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, '');
+    }
 
-function verificaPalavrasDuplicadas(text) {
-    const listaPalavras = text.split(' ');
-    const resultado = {};
-    //objeto[propriedade] = valor;
-    listaPalavras.forEach((palavra) => {
-        if (palavra.length >= 3) {
-            const palavraLimpa = limpaPalavras(palavra);
-            resultado[palavraLimpa] = (resultado[palavraLimpa] || 0) + 1
-        }
-    })
-    return resultado;
-}
+    function verificaPalavrasDuplicadas(text) {
+        const listaPalavras = text.split(' ');
+        const resultado = {};
+        //objeto[propriedade] = valor;
+        listaPalavras.forEach((palavra) => {
+            if (palavra.length >= 3) {
+                const palavraLimpa = limpaPalavras(palavra);
+                resultado[palavraLimpa] = (resultado[palavraLimpa] || 0) + 1
+            }
+        })
+        return resultado;
+    }
+
 
