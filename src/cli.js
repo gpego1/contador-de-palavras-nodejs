219a96c8bd6499ca9ac30d2050e4c51b6e3eaf52
gpego1 <gpego78@gmail.com>
2026-01-16 00:45:05 -0300

    feat commander

--- src/cli.js
+++ src/cli.js
@@ -1,21 +1,66 @@
 import fs from 'fs';
+import path from 'path';
+import { Command } from "commander";
 import trataErros from './erros/funcoesErro.js';
 import { contaPalavras } from './index.js';
 import { montaSaidaArquivo } from "./helpers.js";
+import chalk from 'chalk';
 
-const filesPath = process.argv;
-const link = filesPath[2];
-const endereco = filesPath[3];
+const program = new Command();
 
-fs.readFile(link, 'utf-8', (err, text) => {
-    try{
-        if (err) throw err;
-        const resultado = contaPalavras(text);
-        criaESalvaArquivo(resultado, endereco);
-    } catch(err){
-        trataErros(err);
-    }
-})
+program
+    .version('0.0.1')
+    .option('-t, --texto <string>', 'caminho do texto a ser processado')
+    .option('-d, --destino <string>', 'caminho da pasta onde salvar o arquivo de resultados')
+    .action((options) => {
+        const { texto, destino } = options;
+        if(!texto || !destino) {
+            console.error(chalk.red("Cannot resolve texto or file path"))
+            program.help();
+            return;
+        }
+
+        const caminhoTexto = path.resolve(texto);
+        const caminhoDestino = path.resolve(destino);
+
+        try {
+            processaArquivo(caminhoTexto, caminhoDestino);
+            console.log(chalk.green("texto processado com sucesso!"))
+        } catch (error) {
+            console.log(chalk.red(error));
+        }
+    })
+
+program.parse();
+
+function processaArquivo(texto, destino) {
+    fs.readFile(texto, 'utf-8', (err, text) => {
+        try{
+            if (err) throw err;
+            const resultado = contaPalavras(text);
+            criaESalvaArquivo(resultado, destino);
+        } catch(err){
+            trataErros(err);
+        }
+    })
+}
+
+function criaESalvaArquivo(listaPalavras, endereco) {
+    const arquivoNovo = `${endereco}/resultado.txt`;
+    const textoPalavras = montaSaidaArquivo(listaPalavras);
+
+    fs.promises.writeFile(arquivoNovo, textoPalavras)
+        .then(() => {
+            // processamento feito com o resultado da promessa
+            console.log(chalk.green('Arquivo criado com sucesso!'))
+        })
+        .catch((erro) => {
+            throw erro;
+    })
+        .finally(() => console.log(chalk.blue('Operação finalizada')))
+
+}
+
 
 // async function criaESalvaArquivo(listaPalavras, endereco) {
 //     const arquivoNovo = `${endereco}/resultado.txt`;
@@ -27,19 +72,3 @@ fs.readFile(link, 'utf-8', (err, text) => {
 //         throw erro;
 //     }
 // }
-
-function criaESalvaArquivo(listaPalavras, endereco) {
-    const arquivoNovo = `${endereco}/resultado.txt`;
-    const textoPalavras = montaSaidaArquivo(listaPalavras);
-
-    fs.promises.writeFile(arquivoNovo, textoPalavras)
-        .then(() => {
-            // processamento feito com o resultado da promessa
-            console.log('Arquivo criado com sucesso!')
-        })
-        .catch((erro) => {
-            throw erro;
-    })
-        .finally(() => console.log('Operação finalizada'))
-
-}
